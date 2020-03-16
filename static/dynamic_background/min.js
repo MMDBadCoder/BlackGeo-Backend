ff418ac7f96abcaadc5996f1c6ed1da2f4da7245
MMDBadCoder <gitmmdgit@gmail.com>
2020-03-16 19:56:41 +0330

    forgot password and back of accounting

--- static/dynamic_background/min.js
+++ static/dynamic_background/min.js
@@ -442,21 +442,21 @@ function init() {
                 "color": {"value": "#66B5FF"},
                 "shape": {
                     "type": "circle",
-                    "stroke": {"width": 0, "color": "#000000"},
+                    "stroke": {"width": 1, "color": "#50c7c79c"},
                     "polygon": {"nb_sides": 5},
                     "image": {"src": "img/github.svg", "width": 100, "height": 100}
                 },
                 "opacity": {
-                    "value": 0.5,
+                    "value": 1,
                     "random": false,
                     "anim": {"enable": false, "speed": 1, "opacity_min": 0.1, "sync": false}
                 },
                 "size": {
-                    "value": 4,
+                    "value": 3,
                     "random": true,
                     "anim": {"enable": false, "speed": 40, "size_min": 0.1, "sync": false}
                 },
-                "line_linked": {"enable": true, "distance": 150, "color": "#aceaf9", "opacity": 0.4, "width": 1},
+                "line_linked": {"enable": true, "distance": 150, "color": "#aceaf9", "opacity": 0.9, "width": 1},
                 "move": {
                     "enable": true,
                     "speed": 6,
